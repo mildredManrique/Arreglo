54faec8cabc587a645d09fba917814e2363f2301
Mildred <mildred.manrique@outlook.es>
2020-02-09 22:09:40 -0600

    first commit

---
+++ Dia1.js
@@ -0,0 +1,34 @@
+
+var name ="Mildred";
+var age = 23;
+var a = 50;
+var b = 10;
+var Sumar = a + b;
+
+
+/**alert ("My name is " + name + " and have " + age +  " years old"); 
+
+alert ("Sumar " + Sumar);
+
+alert ("Restar " + (a-b));
+
+alert ("Multiplicar " + (a*b));
+
+alert ("Dividir " + (a/b));*/
+
+/*alert ("Modulo " + (5 % 3));  el modulo es el residuo de la operacion*/
+
+/* alert ("Incremento " + a++ );
+ alert ( a) */
+
+/*b--;
+alert ("Decrement " + b);
+b--;
+alert ("Decrement " + b);
+b--;
+ alert ("Decrement " + b++);
+ alert (b)*/
+
+ alert("Exponenciacion " + b ** 3);
+ 
+
---
+++ Operators.js
@@ -0,0 +1,58 @@
+
+var name ="Mildred";
+var age = 23;
+var a = 50;
+var b = 10;
+
+/*alert ("Sumar " + Sumar);
+
+alert ("Restar " + (a-b));
+
+alert ("Multiplicar " + (a*b));
+
+alert ("Dividir " + (a/b));*/
+
+/*alert ("Modulo " + (5 % 3));  el modulo es el residuo de la operacion*/
+
+/* alert ("Incremento " + a++ );
+ alert ( a) */
+
+/*b--;
+alert ("Decrement " + b);
+b--;
+alert ("Decrement " + b);
+b--;
+ alert ("Decrement " + b++);
+ alert (b)*/
+
+ //alert("Exponenciacion " + b ** 3);
+
+
+//OPERATIORS
+var x = 4;
+var y = 2;
+var z = 7;
+var w ="4";
+
+//z += x; // El valor que se tenia anterior se le suma el valor asignado.
+//z -= y; // El resultado es 5
+//z *= x; // el resultado es 28
+//z /= y; // El resultado es 3.5
+//z %= y; //El modulo es el residuo de la operacion
+//z **= x; //El resultado es el valor de z multiplicado por las veces del valor de x
+
+
+//COMPARISON OPERATOR
+
+var comparison = null;
+
+//comparison = x == 4; // == significa igual a
+comparison = w === 4; //El === es que sea del mismo valor y el mismo tipo (String o varchar) para que el resultado sea true de lo contrario seria false.
+
+
+
+
+
+
+ document.getElementById("Operators").innerHTML = comparison;
+
